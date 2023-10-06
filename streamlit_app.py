7b6e4fba2bde4b54763ab7e0d467eb34a0f567a6
NaveenSF01 <147127800+NaveenSF01@users.noreply.github.com>
2023-10-06 11:38:34 +0530

    Update streamlit_app.py
    Changing the index to have the Fruit column name in the list index

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,6 +13,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit') # Changing the index to have the Fruit coulmn name in the list index
 
 # Multiselect Picklist
 streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
