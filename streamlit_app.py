ea740fe68ff3572945994176a43b13bd64371923
NaveenSF01 <147127800+NaveenSF01@users.noreply.github.com>
2023-10-09 11:42:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,9 +28,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 # streamlit.text(fruityvice_response) # displays response time
- streamlit.text(fruityvice_response.json()) # to show the json
+streamlit.text(fruityvice_response.json()) # to show the json
 
 # write your own comment -what does the next line do? 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
-# streamlit.dataframe(fruityvice_normalized)
+streamlit.dataframe(fruityvice_normalized)
