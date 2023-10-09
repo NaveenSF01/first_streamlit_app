67e32dd6a68f73b6606427a1b2a3c42e642809ec
NaveenSF01 <147127800+NaveenSF01@users.noreply.github.com>
2023-10-09 11:52:32 +0530

    Update streamlit_app.py
    added code to get input from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,11 +27,14 @@ streamlit.dataframe(fruit_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response) # displays response time
 # streamlit.text(fruityvice_response.json())
 
-# write your own comment -what does the next line do? 
+# displayes json version and normlaise it
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
+# Output as a table
 streamlit.dataframe(fruityvice_normalized)
