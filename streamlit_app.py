f8449f5ba853ed129818f92b6d32cb01d3e60a16
NaveenSF01 <147127800+NaveenSF01@users.noreply.github.com>
2023-10-09 11:48:52 +0530

    Update streamlit_app.py
    adding a variable to the API link

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,7 @@ streamlit.dataframe(fruit_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 streamlit.text(fruityvice_response) # displays response time
 # streamlit.text(fruityvice_response.json())
 
