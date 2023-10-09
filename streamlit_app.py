a969fe536b498e4321f6cc7407cf256c8ed1e152
NaveenSF01 <147127800+NaveenSF01@users.noreply.github.com>
2023-10-09 16:18:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -67,7 +67,7 @@ def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
       my_cur.execute("INSERT into fruit_load_list VALUES('" + new_fruit + "')")
       return "Thanks for adding " + new_fruit
-         my_cnx.close()
+      my_cnx.close()
 
 
 
